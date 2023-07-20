a9ea2fbf2dc1b7c384cf252d453d7b985de380ac
Renan Gustavo <renandevfullstack@gmail.com>
2023-07-20 18:39:44 -0300

    ajustes

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,40 +1,69 @@
-// JSON Server module
-const jsonServer = require('json-server');
-const { exec } = require('child_process');
+// // JSON Server module
+// const jsonServer = require('json-server');
+// const { exec } = require('child_process');
 
-const server = jsonServer.create();
-const router = jsonServer.router('server.json');
+// const server = jsonServer.create();
+// const router = jsonServer.router('server.json');
+
+// // Inicie o servidor Vite
+// const viteProcess = exec('npm run dev'); 
+
+// // Make sure to use the default middleware
+// const middlewares = jsonServer.defaults();
+
+// server.use(middlewares);
+// // Add this before server.use(router)
+// // server.use(
+// // Add custom route here if needed
+// // jsonServer.rewriter({
+// //     '/api/*': '/$1',
+// // })
+// // );
+// server.use('/api', router);
+
+
+// server.get('*', (req, res) => {
+//     res.sendFile(__dirname + '/index.html');
+// });
+
+// // Listen to port
+// server.listen(3000, () => {
+//     console.log('JSON Server is running');
+// });
+
+// process.on('SIGINT', () => {
+//     viteProcess.kill('SIGINT');
+//     server.close();
+// });
+
+// // Export the Server API
+// module.exports = server;
+
+const { exec } = require('child_process');
+const jsonServer = require('json-server');
+const path = require('path');
+
+// Caminho para a pasta 'dist' onde o Vite irá gerar o build
+const viteBuildPath = path.join(__dirname, 'dist');
 
 // Inicie o servidor Vite
-const viteProcess = exec('npm run dev'); 
+const viteProcess = exec('vite build && vite preview', { cwd: viteBuildPath });
 
-// Make sure to use the default middleware
+// Inicie o json-server
+const jsonServerProcess = jsonServer.create();
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
-server.use(middlewares);
-// Add this before server.use(router)
-// server.use(
-// Add custom route here if needed
-// jsonServer.rewriter({
-//     '/api/*': '/$1',
-// })
-// );
-server.use('/api', router);
+jsonServerProcess.use(middlewares);
+jsonServerProcess.use('/api', router);
 
-
-server.get('*', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+const PORT = process.env.PORT || 5000;
+jsonServerProcess.listen(PORT, () => {
+    console.log(`JSON Server is running on port ${PORT}`);
 });
 
-// Listen to port
-server.listen(3000, () => {
-    console.log('JSON Server is running');
-});
-
+// Finalize os processos Vite e json-server ao final do script
 process.on('SIGINT', () => {
     viteProcess.kill('SIGINT');
-    server.close();
+    jsonServerProcess.close();
 });
-
-// Export the Server API
-module.exports = server;

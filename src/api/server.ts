fb53e32a3fd5d44d4e31aa20007079eebd623349
Renan Gustavo <renandevfullstack@gmail.com>
2023-07-20 18:58:11 -0300

    ajustes

--- src/api/server.ts
+++ src/api/server.ts
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 app.use('/api', middlewares, router);
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', function (req, res) {
+app.get('/*', function (req: any, res: any) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 

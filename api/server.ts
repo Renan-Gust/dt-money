d9673bde5e878eb8dc0153c9f43f853a5d4d1bf4
Renan Gustavo <renandevfullstack@gmail.com>
2023-07-20 18:08:54 -0300

    ajustes

--- api/server.ts
+++ api/server.ts
@@ -1,15 +1,21 @@
+// JSON Server module
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('../server.json');
+const router = jsonServer.router('server.json');
+
+// Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 // Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}));
+server.use(
+    // Add custom route here if needed
+    jsonServer.rewriter({
+        '/api/*': '/$1',
+    })
+);
 server.use(router);
+// Listen to port
 server.listen(3000, () => {
     console.log('JSON Server is running');
 });
